466de4557f9629b94fc645617618018639789420
David Headrick <davidheadrick1@gmail.com>
2020-01-11 16:13:34 -0500

    updated file

--- week01/week01.js
+++ week01/week01.js
@@ -1,4 +1,4 @@
 function showCongratulations(theButtonClicked) {
-    document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update!</p>"
+    document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update.</p>"
     theButtonClicked.innerHTML = "Click Me Again"
 }
