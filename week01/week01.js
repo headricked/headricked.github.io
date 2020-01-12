cac4989e83a74d988cda2303ee8f86f767aabdc8
David Headrick <davidheadrick1@gmail.com>
2020-01-11 23:17:13 -0500

    updated files

--- week01/week01.js
+++ week01/week01.js
@@ -15,3 +15,33 @@ function showCongratulations(theButtonClicked) {
         document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update!</p>";
     }
 }
+
+function loadStory() {
+    // assign the value of the element name_input to storyName
+    var storyName = document.getElementById("name_input").value;
+
+    // assign the value of storyName from local storage to storyHTML
+    var storyHTML = localStorage.getItem(storyName);
+
+    // assign the value of storyHTML to the value of the element of ID story_editor
+    document.getElementById("story_editor").value = storyHTML;
+}
+
+function saveStory() {
+    // assign the value of the element of ID name_input to storyName
+    var storyName = document.getElementById("name_input").value;
+
+    // assign the value of the element of id story_editor to storyHTML
+    var storyHTML = document.getElementById("story_editor").value;
+
+    // assign the value of storyHTML to the storyName
+    localStorage.setItem(storyName, storyHTML);
+}
+
+function displayStory() {
+    // assign the value of the element with ID story_editor to storyHTML
+    var storyHTML = document.getElementById("story_editor").value;
+
+    // assign the value of storyHTML to the value of the element of ID story_display
+    document.getElementById("story_display").innerHTML = storyHTML;
+}
