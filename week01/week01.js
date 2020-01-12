8145b718307c91c33cd776d5a1355ebc1067a8ce
David Headrick <davidheadrick1@gmail.com>
2020-01-11 21:38:16 -0500

    updated file

--- week01/week01.js
+++ week01/week01.js
@@ -1,4 +1,17 @@
+// function showCongratulations(theButtonClicked) {
+//     document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update!</p>";
+//     theButtonClicked.innerHTML = "Click Me Again";
+// }
+
 function showCongratulations(theButtonClicked) {
-    document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update!</p>"
-    theButtonClicked.innerHTML = "Click Me Again"
+    var x = document.getElementById("result_display");
+    // alert("display: " + x);
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+        theButtonClicked.innerHTML = "Click Me Again";
+        document.getElementById("result_display").innerHTML = "Congratulations, you did it! <p>You got the page to update!</p>";
+    }
 }
