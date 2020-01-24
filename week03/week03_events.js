42e9e51e811fe800c961887f246e8bd82198e666
David Headrick <davidheadrick1@gmail.com>
2020-01-24 18:26:43 -0500

    updated file

--- week03/week03_events.js
+++ week03/week03_events.js
@@ -2,8 +2,20 @@
 //     console.log("Something happened!");
 // }
 
-function doSomething(event) {
-    console.log(event.type);
+// function doSomething(event) {
+//     console.log(event.type);
+// }
+
+// function doSomething(event){
+//     console.log(event.target);
+// }
+
+function doSomething(event){
+    console.log(`   
+                    screen: (${event.screenX},${event.screenY}),
+                    page: (${event.pageX},${event.pageY}),
+                    client: (${event.screenX},${event.screenY})
+                `)
 }
 
 addEventListener("click", doSomething);
