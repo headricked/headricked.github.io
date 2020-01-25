16675175b06ec5403da013003ac7bc05fbc129a6
David Headrick <davidheadrick1@gmail.com>
2020-01-25 12:04:00 -0500

    added files

---
+++ week03/shoppingCart/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Smartphones</title>
+</head>
+<body>
+
+    <section>
+        <table>
+            <tr>
+                <td>
+                    Samsung Galaxy
+                </td>
+                <td>
+                    Apple iPhone
+                </td>
+                <td>
+                    Google Pixel
+                </td>
+                <td>
+                    Motorola G7
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="">Add to Cart</a>
+                </td>
+                <td>
+                    <a href="">Add to Cart</a>
+                </td>
+                <td>
+                    <a href="">Add to Cart</a>
+                </td>
+                <td>
+                    <a href="">Add to Cart</a>
+                </td>
+            </tr>
+        </table>
+
+        <a href="">View Cart</a>
+
+    </section>
+    
+</body>
+</html>
