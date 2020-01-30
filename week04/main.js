75b0ac1f397bbab19b8dce08c41c90df52a7e6e2
David Headrick <davidheadrick1@gmail.com>
2020-01-30 18:56:34 -0500

    updated file

--- week04/main.js
+++ week04/main.js
@@ -1,1 +1,7 @@
-const form = document.forms['search'];
+const form = document.forms[0];
+// const form = document.getElementsByTagname('form')[0];
+// const form = document.forms.search;
+// const form = document.forms['search'];
+// const [input,button] = form.elements;
+
+console.log("form: " + form);
