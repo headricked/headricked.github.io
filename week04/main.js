4249a771f824857fad6865915be2808c7efcb0a4
David Headrick <davidheadrick1@gmail.com>
2020-01-30 19:12:28 -0500

    updated file

--- week04/main.js
+++ week04/main.js
@@ -1,7 +1,10 @@
-const form = document.forms[0];
+// const form = document.forms[0];
 // const form = document.getElementsByTagname('form')[0];
 // const form = document.forms.search;
 // const form = document.forms['search'];
 // const [input,button] = form.elements;
+// const [input,button] = form.elements;
+// console.log("form: " + form);
 
-console.log("form: " + form);
+const input = form.elements.searchInput;
+input.addEventListener('focus', () => alert('focused'), false);
