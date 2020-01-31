e0154877ce51055827ffc39dd3293a632009daae
David Headrick <davidheadrick1@gmail.com>
2020-01-30 20:05:39 -0500

    added and updated files

--- week04/main.js
+++ week04/main.js
@@ -1,10 +1,59 @@
 // const form = document.forms[0];
 // const form = document.getElementsByTagname('form')[0];
 // const form = document.forms.search;
-// const form = document.forms['search'];
-// const [input,button] = form.elements;
+const form = document.forms['search'];
 // const [input,button] = form.elements;
 // console.log("form: " + form);
 
 const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'), false);
+
+// form events
+input.addEventListener('focus',  () => console.log('focused'), false);
+input.addEventListener('blur',   () => console.log('blurred'), false);
+input.addEventListener('change', () => console.log('changed'), false);
+
+
+// submitting a form
+form.addEventListener ('submit', search, false);
+
+// function search() {
+//     alert("Form Submitted");
+//     event.preventDefault();
+// };
+
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+
+input.value = 'Search Here';
+
+input.addEventListener(
+    // first param
+    'focus',
+    
+    // second param
+    function(){
+        if (input.value==='Search Here') {
+            input.value = ''
+        }
+    },
+    
+    // third param
+    false
+);
+
+input.addEventListener(
+    // first param
+    'blur', 
+    
+    // second param
+    function(){
+        if(input.value === '') {
+            input.value = 'Search Here';
+        }
+    },
+    
+    // third param
+    false
+);
