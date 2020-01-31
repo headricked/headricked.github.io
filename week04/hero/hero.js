d97cf2da6289fd8199ed1174fc60cb4b131c26c1
David Headrick <davidheadrick1@gmail.com>
2020-01-30 20:20:48 -0500

    updated files

--- week04/hero/hero.js
+++ week04/hero/hero.js
@@ -5,7 +5,16 @@ function makeHero(event) {
     event.preventDefault(); // prevent the form from being submitted
     const hero = {}; // create an empty object
     hero.name = form.heroName.value; // create a name property based on the input field's value
-    hero.realName = form.realName.value;
+    hero.realName = form.realName.value; // create a real name property based on the input field's value
+
+    // create a super powers property based on the checked checkbox values
+    hero.powers = [];
+    for (let i = 0; i < hero.powers.length; i++) {
+        if (hero.powers[i].checked) {
+            hero.powers.push(form.powers[i].value);    
+        }
+    };
+
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
 }
