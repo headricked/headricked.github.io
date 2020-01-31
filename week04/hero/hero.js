6e633daa3cb8045492a241e0e8faa888d89ee952
David Headrick <davidheadrick1@gmail.com>
2020-01-30 20:11:28 -0500

    added and updated files

--- week04/hero/hero.js
+++ week04/hero/hero.js
@@ -5,6 +5,7 @@ function makeHero(event) {
     event.preventDefault(); // prevent the form from being submitted
     const hero = {}; // create an empty object
     hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.realName = form.realName.value;
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
 }
