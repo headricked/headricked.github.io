99d399909b9910c8f7d1bfb7735439c66f8fc7b3
David Headrick <davidheadrick1@gmail.com>
2020-01-30 21:06:06 -0500

    updated files

--- week04/hero/hero.js
+++ week04/hero/hero.js
@@ -16,7 +16,23 @@ function makeHero(event) {
         }
     };
 
+    // more succinct way of writing the loop
     // hero.powers = [...form.powers].filter(box => box.checked).map(box => box.value);
+
+    // create a person type property based on the checked checkbox values
+    hero.category = [];
+    for (let i = 0; i < form.category.length; i++) {
+        if (form.category[i].checked) {
+            // push the checked value onto the hero.powers array
+            hero.category.push(form.category[i].value);
+        }
+    };
+
+    // more succinct way of writing the loop
+    // hero.category = [...form.category].filter(box => box.checked).map(box => box.value);
+
+    // create an age property based on the input field's value
+    hero.age = form.age.value;
 
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
     return hero;
