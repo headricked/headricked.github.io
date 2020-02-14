533f0ba5042419492b59d92b940bc509a76a9bb9
David Headrick <davidheadrick1@gmail.com>
2020-02-14 12:14:40 -0500

    updated files

--- week06/week06.js
+++ week06/week06.js
@@ -4,32 +4,70 @@ let students = [];
 
 if (localStorage.getItem('students')) {
     students = JSON.parse(localStorage.getItem('students'));
-} else {
-    loadStudents();
 }
 
-saveStudent(students);
-console.table(students);
+// ATTACH EVENTLISTENER TO ADD STUDENT BUTTON
+document.querySelector('#btn-add-student').addEventListener('click', addStudent, false);
 
-// LOAD STUDENT
-function loadStudents() {
+// window.addStudent = addStudent;
 
-    const student1 = new Student( 'David', 'Headrick', '11-111-1111' );
-    const student2 = new Student( 'Lindsey', 'Headrick', '22-222-2222' );
-    const student3 = new Student( 'Jane', 'Headrick', '33-333-3333');
+// ADD STUDENT
+function addStudent() {
+    // GET VALUES OF FORM FIELDS AND SAVE INTO NEW STUDENT OBJECT
+    const newStudent = new Student(students) (
+        document.getElementById('firstName').value,
+        document.getElementById('lastName').value,
+        document.getElementById('iNumber').value
+    );
 
-    students.push(student1);
-    students.push(student2);
-    students.push(student3);
-    
+    // ADD STUDENT TO STUDENTS ARRAY
+    students.push(newStudent);
+
+    // SAVE TO LOCAL STORAGE
     saveStudent(students);
-    
+
+    // POPULATE THE TABLE
+    loadStudents();
+
+    // CLEAR FORM FIELDS
+    clearFields();
+}
+
+
+// CLEAR FORM FIELDS FOLLOWING SUCCESSFUL SUBMISSION
+function clearFields() {
+    document.querySelector('firstName').value = '';
+    document.querySelector('lastName').value = '';
+    document.querySelector('iNumber').value = '';
+}
+
+// CLEAR THE TABLE OF DATA
+function clearStudents() {
+    let table = document.querySelector('tbody');
+    while (table.rows.length > 0) {
+        table.deleteRow(0);
+    }
+}
+
+// LOAD STUDENT INTO HTML TABLE
+function loadStudents() {
+    clearStudents();
+
+    students.forEach(
+        (student) => {
+            document.querySelector('tbody').innerHTML +=
+            `
+                <tr>
+                    <td>${ student.firstName }</td>
+                    <td>${ student.lastName }</td>
+                    <td>${ student.iNumber }</td>
+                </tr>
+            `;
+        }
+    );
 }
 
 // SAVE STUDENT
 function saveStudent(students) {
     localStorage.setItem('students', JSON.stringify(students));
 }
-
-
-// console.table(students);
