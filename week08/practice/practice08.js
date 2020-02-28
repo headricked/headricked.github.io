8c802f691cc699230c606356af8c35f38d175e21
David Headrick <davidheadrick1@gmail.com>
2020-02-28 00:03:34 -0500

    new file

--- week08/practice/practice08.js
+++ week08/practice/practice08.js
@@ -4,3 +4,14 @@ context.strokeStyle = "red";
 context.fillStyle = "rgba(0, 0, 255, 0.5)";
 context.fillRect(10, 10, 100, 100);   
 context.strokeRect(10, 10, 100, 100);
+
+function drawPattern() {
+    var img = new Image();
+    img.src = "HeadrickDavid.jpg";
+    img.onload = function() {
+    var pattern = context.createPattern(img, "repeat"); 
+    context.fillStyle = pattern;                        
+    context.fillRect(10, 10, 100, 100);                  
+    context.strokeRect(10, 10, 100, 100);             
+    };
+}
