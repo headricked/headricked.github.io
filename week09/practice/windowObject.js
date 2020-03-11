451abd1d41c4936932a92847d1e3d14f9e98dfb2
David Headrick <davidheadrick1@gmail.com>
2020-03-10 21:22:51 -0400

    updated files

--- week09/practice/windowObject.js
+++ week09/practice/windowObject.js
@@ -1,10 +1,10 @@
-window.alert('Hi. After you click OK, you may see a pop-up to the home page of the Church of Jesus Christ of Latter-day Saints.');
-
-const popup = window.open('https://www.churchofjesuschrist.org/?lang=eng','Church of Jesus Christ of Latter-day Saints','width=400,height=400,resizable=yes');
-
-
+// ADD EVENT LISTENERS
 document.querySelector('#btn-screen-dimensions').addEventListener('click', showScreenDimensions, false);
-    function showScreenDimensions() {
+document.querySelector('#btn-make-cookie').addEventListener('click', makeCookie, false);
+
+
+function showScreenDimensions() {
+    
     let screenHeight, screenWidth;
 
     screenHeight = window.screen.height;
@@ -12,3 +12,17 @@ document.querySelector('#btn-screen-dimensions').addEventListener('click', showS
 
     alert(`Screen height: ${screenHeight}, Screen width: ${screenWidth}`);
 }
+
+function makeCookie() {
+    
+}
+
+window.alert('Hi. After you click OK, you may see a pop-up to the home page of the Church of Jesus Christ of Latter-day Saints.');
+
+const popup = window.open('https://www.churchofjesuschrist.org/?lang=eng','Church of Jesus Christ of Latter-day Saints','width=400,height=400,resizable=yes');
+
+
+
+document.cookie = 'name=Batman'
+document.cookie = 'city=Gotham'
+
