ec164ed1c2cd35fcfac41ab62e9b3fb43192a1cc
David Headrick <davidheadrick1@gmail.com>
2020-03-10 21:16:46 -0400

    updated files

--- week09/practice/windowObject.js
+++ week09/practice/windowObject.js
@@ -1,14 +1,14 @@
 window.alert('Hi. After you click OK, you may see a pop-up to the home page of the Church of Jesus Christ of Latter-day Saints.');
 
-const popup = window.open(
-    'https://www.churchofjesuschrist.org/?lang=eng',
-    'Church of Jesus Christ of Latter-day Saints',
-    'width=400,height=400,resizable=yes'
-    );
+const popup = window.open('https://www.churchofjesuschrist.org/?lang=eng','Church of Jesus Christ of Latter-day Saints','width=400,height=400,resizable=yes');
 
-let screenHeight, screenWidth;
 
-screenHeight = window.screen.height;
-screenWidth = window.screen.width;
+document.querySelector('#btn-screen-dimensions').addEventListener('click', showScreenDimensions, false);
+    function showScreenDimensions() {
+    let screenHeight, screenWidth;
 
-alert(`Screen height: ${screenHeight}, Screen width: ${screenWidth}`);
+    screenHeight = window.screen.height;
+    screenWidth = window.screen.width;
+
+    alert(`Screen height: ${screenHeight}, Screen width: ${screenWidth}`);
+}
