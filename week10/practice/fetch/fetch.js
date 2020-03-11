85730f47f93fa72c236c29a47ea63dbed2163f00
David Headrick <davidheadrick1@gmail.com>
2020-03-10 23:54:44 -0400

    updated files

--- week10/practice/fetch/fetch.js
+++ week10/practice/fetch/fetch.js
@@ -1,7 +1,30 @@
-fetch('https://swapi.co/api/people/1/')
+let url = 'https://swapi.co/api/people';
+
+fetch(url)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         console.log(data);
+
+
+        let table = '<table>';
+
+        data.results.forEach(result => {
+            table += '<tr>';
+            table += '<td class="name">' + result.name + '</td>';
+            table += '<td class="hideable">' + result.height + '</td>';
+            table += '<td class="hideable">' + result.mass + '</td>';
+            table += '<td class="hideable">' + result.hair_color + '</td>';
+            table += '<td class="hideable">' + result.skin_color + '</td>';
+            table += '<td class="hideable">' + result.eye_color + '</td>';
+            table += '<td class="hideable">' + result.birt_hyear + '</td>';
+            table += '<td class="hideable">' + result.gender + '</td>';
+            table += '</tr>'
+        })
+        
+        table += '</table>';
+
+
     });
+
