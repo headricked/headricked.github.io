3c22d7b8827004f2be1a365e6baf868e29bdbb8d
David Headrick <davidheadrick1@gmail.com>
2020-04-04 19:24:12 -0400

    added styles

--- week07/practice/main.js
+++ week07/practice/main.js
@@ -1,10 +1,8 @@
 const textButton = document.getElementById('person');
-const apiButton = document.getElementById('number');
 const outputDiv = document.getElementById('output');
 
 // const textURL = 'http://numbersapi.com/random';
 const textURL = 'https://randomuser.me/api/?results=1';
-const apiURL = 'http://numbersapi.com/random';
 
 // textButton.addEventListener('click', () => {
 //     fetch(textURL)
@@ -35,16 +33,17 @@ textButton.addEventListener('click', () => {
 },false);
 
 
-apiButton.addEventListener('click', () => {
-    fetch(apiURL)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        return response;
-    }
-    throw Error(response.statusText);
-    })
-    .then( response => response.json() )
-    .then( data => outputDiv.innerText = data.value )
-    .catch( error => console.log('There was an error:', error))
-},false);
+// apiButton.addEventListener('click', () => {
+//     fetch(apiURL)
+//     .then( response => {
+//         outputDiv.innerHTML = 'Waiting for response...';
+//     if(response.ok) {
+//         console.log(response);
+//         return response;
+//     }
+//     throw Error(response.statusText);
+//     })
+//     .then( response => response.json() )
+//     .then( data => outputDiv.innerText = data.value )
+//     .catch( error => console.log('There was an error:', error))
+// },false);
